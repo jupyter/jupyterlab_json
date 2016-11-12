0e182c17870c14c29633b9fa81641ed84f1681b8
Grant Nestor <grantnestor@gmail.com>
2016-11-12 15:15:20 -0800

    Add filter

--- src/component.tsx
+++ src/component.tsx
@@ -7,53 +7,136 @@ import * as ReactDOM from 'react-dom';
 
 import JSONTree from 'react-json-tree';
 
-export
-function renderComponent(json: [any] | {}, node: Element): React.Component<{}, React.ComponentState> | Element | void {
-  return ReactDOM.render(
-    <JSONTree
-      data={json}
-      theme={{
-        extend: 'default',
-        // TODO: Use Jupyter Lab's current CodeMirror theme vs. 'cm-s-jupyter'
-        tree: 'CodeMirror cm-s-jupyter',
-        // valueLabel: 'cm-variable',
-        valueText: 'cm-string',
-        // nestedNodeLabel: 'cm-variable-2',
-        nestedNodeItemString: 'cm-comment',
-        // value: {},
-        // label: {},
-        // itemRange: {},
-        // nestedNode: {},
-        // nestedNodeItemType: {},
-        // nestedNodeChildren: {},
-        // rootNodeChildren: {}
-      }}
-      labelRenderer={([label, type]) => {
-        switch (type) {
-          case 'array':
-            return <span className="cm-variable-2">{label}: </span>;
-          case 'object':
-            return <span className="cm-variable-3">{label}: </span>;
-          case 'root':
-          default:
-            return <span className="cm-variable">{label}: </span>;
-        }
-      }}
-      valueRenderer={(raw) => {
-        switch (typeof raw) {
-          case 'number':
-            return <span className="cm-number">{raw}</span>;
-          case 'string':
-          default:
-            return <span className="cm-string">{raw}</span>;
-        }
-      }}
-    />,
-    node
-  );
+import * as Highlight from 'react-highlighter';
+
+import {
+  JSONValue
+} from 'phosphor/lib/algorithm/json';
+
+export interface JSONComponentProps {
+  data: JSONValue;
+}
+
+export interface JSONComponentState {
+  filter: string;
 }
 
 export
-function disposeComponent(node: Element): void {
-  ReactDOM.unmountComponentAtNode(node);
+class JSONComponent extends React.Component<JSONComponentProps, JSONComponentState> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: ''
+    };
+  }
+
+  public componentDidMount() {
+    /**
+     * Stop propagation of keyboard events to JupyterLab to prevent triggering 
+     * keyboard shortcuts. 
+     */
+    ReactDOM.findDOMNode(this).addEventListener('keydown', (event) => {
+      event.stopPropagation();
+    }, false);
+  }
+
+  public render() {
+    let data = this.props.data;
+    return (
+      <div
+        style={{
+          position: 'relative'
+        }}
+      >
+        <input
+          value={this.state.filter}
+          onChange={(event) => {
+            this.setState({filter: (event.target as any).value});
+          }}
+          style={{
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            width: '33%',
+            maxWidth: 150,
+            zIndex: 10,
+            fontSize: 13,
+            paddingHorizontal: 4,
+            paddingVertical: 2
+          }}
+          type="text"
+          placeholder="Filter..."
+        />
+        <JSONTree
+          data={data}
+          theme={{
+            extend: 'default',
+            // TODO: Use Jupyter Lab's current CodeMirror theme vs. 'cm-s-jupyter'
+            tree: 'CodeMirror cm-s-jupyter',
+            // valueLabel: 'cm-variable',
+            valueText: 'cm-string',
+            // nestedNodeLabel: 'cm-variable-2',
+            nestedNodeItemString: 'cm-comment',
+            // value: {},
+            // label: {},
+            // itemRange: {},
+            // nestedNode: {},
+            // nestedNodeItemType: {},
+            // nestedNodeChildren: {},
+            // rootNodeChildren: {}
+          }}
+          labelRenderer={([label, type]) => {
+            let className;
+            switch (type) {
+              case 'array':
+                className = 'cm-variable-2';
+                break;
+              case 'object':
+                className = 'cm-variable-3';
+                break;
+              case 'root':
+              default:
+                className = 'cm-variable';
+            }
+            return (
+              <span className={className}>
+                <Highlight
+                  search={this.state.filter}
+                  matchStyle={{ backgroundColor: 'yellow' }}
+                >
+                  {`${label}: `}
+                </Highlight>
+              </span>
+            );
+          }}
+          valueRenderer={(raw) => {
+            let className;
+            switch (typeof raw) {
+              case 'number':
+                className = 'cm-number';
+                break;
+              case 'string':
+              default:
+                className = 'cm-string';
+            }
+            return (
+              <span className={className}>
+                <Highlight
+                  search={this.state.filter}
+                  matchStyle={{ backgroundColor: 'yellow' }}
+                >
+                  {raw.toString()}
+                </Highlight>
+              </span>
+            );
+          }}
+          shouldExpandNode={(keyName, data, level) => {
+            return (this.state.filter !== '' && JSON.stringify(data).includes(this.state.filter)) || level < 1;
+          }}
+        />
+      </div>
+    );
+  }
+
 }
