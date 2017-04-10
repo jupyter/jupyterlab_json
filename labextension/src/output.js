b2a7855bed42fbbbc18cffc8609a2c12af5189a4
Grant Nestor <grantnestor@gmail.com>
2017-04-10 12:39:46 -0700

    Merge branch 'cookiecutter' into develop

--- labextension/src/output.js
+++ labextension/src/output.js
@@ -3,67 +3,88 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import JSONComponent from 'jupyterlab_json_react';
 
-/**
- * The class name added to this OutputWidget.
- */
+const MIME_TYPE = 'application/json';
 const CLASS_NAME = 'jp-OutputWidgetJSON';
 
 /**
- * A widget for rendering JSON.
+ * A Phosphor widget for rendering JSON
  */
 export class OutputWidget extends Widget {
   constructor(options) {
     super();
+    this._mimeType = options.mimeType;
+    this._data = options.model.data;
+    this._metadata = options.model.metadata;
     this.addClass(CLASS_NAME);
-    this._data = options.model.data.get(options.mimeType);
-    this._metadata = options.model.metadata.get(options.mimeType);
   }
 
   /**
-   * A message handler invoked on an `'after-attach'` message.
+   * A message handler invoked on an `'after-attach'` message
    */
   onAfterAttach(msg) {
+    /* Render initial data */
     this._render();
   }
 
   /**
-   * A message handler invoked on an `'before-detach'` message.
+   * A message handler invoked on an `'before-detach'` message
    */
   onBeforeDetach(msg) {
+    /* Dispose of resources used by this widget */
     ReactDOM.unmountComponentAtNode(this.node);
   }
 
   /**
-   * A render function given the widget's DOM node.
+   * A message handler invoked on a `'child-added'` message
+   */
+  onChildAdded(msg) {
+    /* e.g. Inject a static image representation into the mime bundle for
+     *  endering on Github, etc. 
+     */
+    // renderLibrary.toPng(this.node).then(url => {
+    //   const data = url.split(',')[1];
+    //   this._data.set('image/png', data);
+    // })
+  }
+
+  /**
+   * A message handler invoked on a `'resize'` message
+   */
+  onResize(msg) {
+    /* Re-render on resize */
+    this._render();
+  }
+
+  /**
+   * Render data to DOM node
    */
   _render() {
-    let json = this._source;
-    ReactDOM.render(
-      <JSONComponent 
-        data={this._data} 
-        metadata={this._metadata} 
-        theme="cm-s-jupyter"
-      />,
-      this.node
-    );
+    const props = {
+      data: this._data.get(this._mimeType),
+      theme: 'cm-s-jupyter',
+      metadata: this._metadata.get(this._mimeType),
+      width: this.node.offsetWidth,
+      height: this.node.offsetHeight
+    };
+    ReactDOM.render(<JSONComponent {...props} />, this.node);
   }
 }
 
 export class OutputRenderer {
   /**
-   * The mime types this OutputRenderer accepts.
+   * The mime types that this OutputRenderer accepts
    */
-  mimeTypes = ['application/json'];
+  mimeTypes = [MIME_TYPE];
 
   /**
-   * Whether the renderer can render given the render options.
+   * Whether the renderer can render given the render options
    */
   canRender(options) {
     return this.mimeTypes.indexOf(options.mimeType) !== -1;
   }
 
   /**
-   * Render the transformed mime bundle.
+   * Render the transformed mime bundle
    */
   render(options) {
     return new OutputWidget(options);
